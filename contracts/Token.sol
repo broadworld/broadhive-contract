e8b0d2c772cce18f471177c471a13e5f286a7e96
"broadworld" <"meshboc@gmail.com">
2022-04-18 23:54:15 +0100

    git commit -m "contract"

---
+++ contracts/Token.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.4.0-solc-0.7/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+
+    constructor () ERC20("Binance Broadhive", "BBHT") {
+        _mint(msg.sender, 1000000000 * (10 ** uint256(decimals())));
+    }
+}
